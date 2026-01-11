223189c6e98c3b14555785a781c3517af5cc0412
minahilhasan <minahilhasan@gmail.com>
2026-01-12 02:35:04 +0500

    Update app.py to scrap website

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from dataretrieval import extract_courses_cached
+from scrapping import scrap_webpage 
+
+def main():
+    st.title("University Course Bot")
+
+    all_data = []
+    urls = [
+        "https://admissions.umt.edu.pk/search.aspx",
+        "https://www.vu.edu.pk/StudyScheme/StudyScheme",
+        "https://logix.edu.pk/course-detail/1/all-academic-programs-by-vu",
+        "https://itu.edu.pk/academics/",
+        "https://pu.edu.pk/program",
+        "https://giki.edu.pk/Programs/",
+        "https://ucp.edu.pk/undergraduate/",
+        "https://ucp.edu.pk/postgraduate/",
+        "https://nust.edu.pk/academics/"
+    ]
+    for url in urls:
+        st.write(f"Scraping: {url} ...")
+        data = scrap_webpage(url)
+        all_data.append(data)
+
+    st.success("Scraping completed!")
+
+if __name__ == "__main__":
+    main()
+
