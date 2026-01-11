cafa78419e3ef7d506fc2c98effd006bd00db4c7
Minahil Hasan <minahilhasan@gmail.com>
2026-01-12 02:31:58 +0500

    Edited scrapping.py to consume less token

--- scrapping.py
+++ scrapping.py
@@ -5,59 +5,34 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-def scrapwebpage():
-    if "api" not in st.session_state:
-        st.session_state.api = Firecrawl(api_key=os.getenv("FIRECRAWL_API_KEY"))
-    firecrawl=st.session_state.api
-    schema = {
-        "type": "object",
-        "properties": {
-            "university_name": {
-                "type": "string"
-            },
-            "courses": {
-                "type": "array",
-                "items": {
-                    "type": "object",
-                    "properties": {
-                        "course_name": {
-                            "type": "string"
-                        },
-                        "general_details": {
-                            "type": "string"
-                        }
-                    }
-                }
-            }
+if "api" not in st.session_state:
+    st.session_state.api = Firecrawl(api_key=os.getenv("FIRECRAWL_API_KEY"))
+
+firecrawl = st.session_state.api
+ 
+schema = {
+    "type": "object",
+    "properties": {
+        "university_name": {"type": "string"},
+        "faculty_name":{"type":"string"},
+        "programs": {
+            "type": "array",
+            "items": {"type": "string"}  
         }
     }
+}
+
+prompt = "Extract university name, faculty names and program names."
 
 
-    prompt = """
-    Extract the university name and all academic programs,
-    with course name, and general details.
-    """
-
-    urls = [
-        "https://nust.edu.pk",
-        "https://lums.edu.pk",
-        "https://uet.edu.pk",
-        "https://comsats.edu.pk",
-        "https://uok.edu.pk",
-        "https://itu.edu.pk",
-        "https://nu.edu.pk",
-        "https://pu.edu.pk",
-        "https://giki.edu.pk",
-        "https://iba.edu.pk",
-
-    ]
-
+@st.cache_data(show_spinner=True)
+def scrap_webpage(url):
     response = firecrawl.extract(
-        urls=urls,
+        urls=[url],
         prompt=prompt,
         schema=schema,
-        enable_web_search=True,
-)
-
-    st.write(response.data)
+        enable_web_search=False,  
+    )
     return response.data
+
+
