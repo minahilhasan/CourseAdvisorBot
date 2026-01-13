ffd3c0686bc6dd39929113b9443db7383ac15af7
Minahil Hasan <minahilhasan@gmail.com>
2026-01-13 19:45:12 +0500

    Called chatbot function

--- app.py
+++ app.py
@@ -1,29 +1,9 @@
 
 import streamlit as st
-from scrapping import scrap_webpage 
+from groqchatbot import chatbot
 
 def main():
-    st.title("University Course Bot")
-
-    all_data = []
-    urls = [
-        "https://admissions.umt.edu.pk/search.aspx",
-        "https://www.vu.edu.pk/StudyScheme/StudyScheme",
-        "https://logix.edu.pk/course-detail/1/all-academic-programs-by-vu",
-        "https://itu.edu.pk/academics/",
-        "https://pu.edu.pk/program",
-        "https://giki.edu.pk/Programs/",
-        "https://ucp.edu.pk/undergraduate/",
-        "https://ucp.edu.pk/postgraduate/",
-        "https://nust.edu.pk/academics/"
-    ]
-    for url in urls:
-        st.write(f"Scraping: {url} ...")
-        data = scrap_webpage(url)
-        st.write(data)
-        all_data.append(data)
-
-    st.success("Scraping completed!")
+    chatbot()
 
 if __name__ == "__main__":
     main()
